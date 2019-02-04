cd9a515b6d0ef4e397c73a42e13e306ee6e2da82
Tatumura <cunyuelongye@murakoshitatsuyanoMacBook-puro.local>
2019-02-04 11:41:05 +0900

    Add create_messages.rb
    モデルを作成、ルーティングを設定

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -19,11 +19,14 @@ class GroupsController < ApplicationController
 
   def update
     if @group.update(group_params)
-      redirect_to group_messaages_path(@group), notice: 'グループを編集しました'
+      redirect_to group_messages_path(@group), notice: 'グループを編集しました'
     else
       render :edit
+    end
+  end
 
   private
+
   def group_params
     params.require(:group).permit(:name, { :user_ids => [] })
   end
