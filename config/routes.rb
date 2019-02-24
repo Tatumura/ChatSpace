41593fad4d21d4c674423f075e25cb1336886256
Tatumura <cunyuelongye@murakoshitatsuyanoMacBook-puro.local>
2019-02-24 13:31:12 +0900

    Routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,8 @@
-
 Rails.application.routes.draw do
   devise_for :users
-<<<<<<< Updated upstream
   root 'groups#index'
-  resources :users, only: [:index, :edit, :update]
+  resource :user, only: [:edit, :update]
   resources :groups, only: [:new, :create, :edit, :update] do
     resources :messages, only: [:index, :create]
   end
-=======
-  root 'messages#index'
-  resources :users, only: [:edit, :update]
->>>>>>> Stashed changes
 end
