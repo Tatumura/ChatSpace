1fee18d63556ec9418a3b721c58e50c56e7bac1c
Tatumura <cunyuelongye@murakoshitatsuyanoMacBook-puro.local>
2019-03-02 14:18:57 +0900

    Add is

--- spec/factories/messages.rb
+++ spec/factories/messages.rb
@@ -1,7 +1,7 @@
 FactoryGirl.define do
   factory :message do
     content Faker::Lorem.sentence
-    image File.open("#{Rails.root}/public/images/no_image.jpg")
+    image File.open("#{Rails.root}/spec/images//IMG_3470.jpg")
     user
     group
   end
